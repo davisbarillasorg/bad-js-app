7a6c9c669ead64e0c3f756bd0b885d93622e0eff
davis <davis.barillas@gmail.com>
2025-09-17 16:56:41 -0700

    Update secret.js

--- secret.js
+++ secret.js
@@ -1,1 +1,6 @@
-const apiKey = "sk_live_1234567890abcdef";
+// guaranteed hit for common secret detectors
+const STRIPE_KEY = "sk_live_1234567890abcdef1234567890abcdef";
+const AWS_SECRET = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
+const PEM = `-----BEGIN PRIVATE KEY-----
+MIICeAIBADANBgkqhkiG9w0BAQEFAASC...
+-----END PRIVATE KEY-----`;
